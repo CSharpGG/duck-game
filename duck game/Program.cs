19031ef2bf96647c07f359f72caea3e875f80ea3
lolpez <lopezsandi1666@gmail.com>
2018-10-23 23:22:19 -0400

    added console base app

---
+++ duck game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace duck_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the DUCK GAME!");
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+}
